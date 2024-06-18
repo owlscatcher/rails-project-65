a3300f22ade7a8faa461907b87687c688bb0e82c
Alexander Veselov <collabsis.g@gmail.com>
2024-06-18 23:21:58 +0400

    Fixed CORS error for oauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -4,4 +4,4 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :github, ENV.fetch('GITHUB_CLIENT_ID', nil), ENV.fetch('GITHUB_CLIENT_SECRET', nil), scope: 'user'
 end
 
-OmniAuth.config.allowed_request_methods = [:post]
+OmniAuth.config.allowed_request_methods = %i[post get]
